a1d34d3d3c8606a429d966ad2b5f8a1cb0098a39
Maksym Hodunov <m.godunov@yandex.ru>
2020-08-16 13:46:09 +0300

    Слайдеры

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ $(document).ready(function () {
     $('.slick-slider').slick({
         dots: true,
         infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+        autoplay: 2000,
+        fade: true,
+        cssEase: "linear"
     });
 });
